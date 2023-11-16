1301c25033912553f4d38405d0f53ab00dbe6f66
edwardrico <120380332+edwardrico@users.noreply.github.com>
2023-11-16 16:48:52 +0100

    gestion d'erreur login register

--- static/js/success.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    // Après 2 secondes (2000 millisecondes), affichez l'icône "check-circle" et masquez l'icône de chargement
-    setTimeout(function() {
-        $("#loading-container").hide();
-        $("#success-container").show();
-    }, 2000);
-});
-
--- user_profile/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from user_profile.models import UserProfile
-
-
-# Formulaire pour le profil d'utilisateur
-class UserProfileForm(forms.ModelForm):
-    class Meta:
-        model = UserProfile
-        fields = ['profile_pic', 'bio', 'location']
